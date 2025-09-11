877d0386de88646962ae6d73638428c4cbadd3e8
Rinat <rinatkram@gmail.com>
2025-09-11 15:57:50 +0300

    Add files via upload

---
+++ KartyPracy2/Program6.cs
@@ -0,0 +1,11 @@
+﻿int a = int.Parse(Console.ReadLine());
+int p = int.Parse(Console.ReadLine());
+
+if ((Math.Pow(a, p) - a) % p == 0)
+{
+    Console.WriteLine("TAK");
+}
+else
+{
+    Console.WriteLine("NIE");
+}
---
+++ KartyPracy2/Program7.cs
@@ -0,0 +1,12 @@
+﻿
+int p = Convert.ToInt32(Console.ReadLine());
+int k = Convert.ToInt32(Console.ReadLine());
+int s = Convert.ToInt32(Console.ReadLine());
+if (p + s * 3 >= k)
+{
+    Console.WriteLine("TAK");
+}
+else
+{
+    Console.WriteLine("NIE");
+}
